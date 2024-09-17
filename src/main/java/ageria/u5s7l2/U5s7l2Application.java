dc8a270ea6e1e7ae3badb7911dbb80a5d0e6a573
Andrea <ageria20@gmail.com>
2024-09-17 13:46:23 +0200

    Initial commit

---
+++ src/main/java/ageria/u5s7l2/U5s7l2Application.java
@@ -0,0 +1,13 @@
+package ageria.u5s7l2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5s7l2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5s7l2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ageria/u5s7l2/U5s7l2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ageria.u5s7l2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5s7l2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
